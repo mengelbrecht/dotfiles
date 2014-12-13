24720bb0ea16928f9763096d55355478bb71529a
Markus Engelbrecht <mgee@users.noreply.github.com>
2014-12-13 10:11:23 +0100

    remove mjolnir config in favor of hammerspoon

--- mjolnir/init.lua
+++
@@ -1,8 +0,0 @@
-package.path = os.getenv("HOME") .. "/.mjolnir/rocks/share/lua/5.2/?.lua;" ..
-               os.getenv("HOME") .. "/.mjolnir/rocks/share/lua/5.2/?/init.lua;" ..
-               os.getenv("HOME") .. "/.mjolnir/?.lua;" ..
-               package.path .. ";./?/init.lua"
-package.cpath = os.getenv("HOME") .. "/.mjolnir/rocks/lib/lua/5.2/?.so;" .. package.cpath
-
-require 'moonscript'
-require 'main'
