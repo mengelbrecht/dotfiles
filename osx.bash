5a4969f1829754f99e9ad3cb366b540c3b4165a2
Markus Engelbrecht <mgee@users.noreply.github.com>
2015-07-31 19:13:20 +0200

    disable quarantine dialog

--- osx.bash
+++ osx.bash
@@ -35,6 +35,9 @@ defaults write NSGlobalDomain NSAutomaticSpellingCorrectionEnabled -bool false
 # Automatically quit printer app once the print jobs complete
 defaults write com.apple.print.PrintingPrefs "Quit When Finished" -bool true
 
+# Disable the “Are you sure you want to open this application?” dialog
+defaults write com.apple.LaunchServices LSQuarantine -bool false
+
 # Disable Resume system-wide
 defaults write com.apple.systempreferences NSQuitAlwaysKeepsWindows -bool false
 
