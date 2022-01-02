c75300a57ef297b60ee68b43ea553e4991b81391
Markus Engelbrecht <git@enbre.com>
2022-01-02 13:44:51 +0100

    fish: Improve homebrew config

--- config/fish/conf.d/10-homebrew.fish
+++ config/fish/conf.d/10-homebrew.fish
@@ -1,6 +1,8 @@
-if test -d "$HOME/.homebrew"
+if test -d "/opt/homebrew-intel"
+  set homebrew "/opt/homebrew-intel"
+else if test -d "$HOME/.homebrew"
   set homebrew "$HOME/.homebrew"
-else if -f "/usr/local/bin/brew"
+else if test -f "/usr/local/bin/brew"
   set homebrew "/usr/local"
 end
 
@@ -11,8 +13,14 @@ if test -d $homebrew
         fish_add_path "$homebrew/opt/coreutils/libexec/gnubin"
     end
 
+    if test -d "$homebrew/opt/openjdk/bin"
+        fish_add_path "$homebrew/opt/openjdk/bin"
+    end
+
     set -gx HOMEBREW_NO_ANALYTICS "1"
     set -gx HOMEBREW_NO_ENV_HINTS "1"
+    set -gx HOMEBREW_INSTALL_BADGE " "
+    set -gx HOMEBREW_BUNDLE_FILE "$XDG_CONFIG_HOME/Brewfile"
 
     alias brewup "brew update && brew upgrade && brew cleanup"
 end
