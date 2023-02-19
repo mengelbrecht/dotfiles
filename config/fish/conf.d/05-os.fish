70be05cabd0eedbeff298fa9574eca3da0cd897f
Markus Engelbrecht <git@enbre.com>
2023-02-19 17:46:33 +0100

    fish: Move code alias to os config section

--- config/fish/conf.d/05-os.fish
+++ config/fish/conf.d/05-os.fish
@@ -2,6 +2,7 @@ switch (uname)
     case Darwin
         alias o open
         alias clearDNS "sudo dscacheutil -flushcache; sudo killall -HUP mDNSResponder"
+        alias code "/Applications/Visual\ Studio\ Code.app/Contents/Resources/app/bin/code"
     case Linux
     case '*'
 end
