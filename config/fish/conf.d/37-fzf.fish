15f501d3fdb2d19bd6e14642d6ec1e7c5b40539b
Markus Engelbrecht <git@enbre.com>
2023-03-08 14:22:20 +0100

    fzf: Use onedark theme

--- config/fish/conf.d/37-fzf.fish
+++ config/fish/conf.d/37-fzf.fish
@@ -1,6 +1,6 @@
 set -gx FZF_DEFAULT_COMMAND 'fd --type f --strip-cwd-prefix'
 set -gx FZF_CTRL_T_COMMAND "$FZF_DEFAULT_COMMAND"
-set -gx FZF_DEFAULT_OPTS '--cycle --layout=reverse --border'
+set -gx FZF_DEFAULT_OPTS '--cycle --layout=reverse --border --color=dark,fg:-1,bg:-1,hl:#c678dd,fg+:#ffffff,bg+:#4b5263,hl+:#d858fe,info:#98c379,prompt:#61afef,pointer:#be5046,marker:#e5c07b,spinner:#61afef,header:#61afef'
 
 if test -f "$homebrew/opt/fzf/shell/key-bindings.fish"
   source "$homebrew/opt/fzf/shell/key-bindings.fish"
