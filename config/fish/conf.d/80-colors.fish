86150d5c2a6834e86a8846fa0905555cc1c044fa
Markus Engelbrecht <git@enbre.com>
2023-03-12 13:47:07 +0100

    fish: Adjust colors

--- config/fish/conf.d/80-colors.fish
+++ config/fish/conf.d/80-colors.fish
@@ -2,13 +2,13 @@ set -g fish_color_normal white
 set -g fish_color_command blue
 set -g fish_color_keyword magenta
 set -g fish_color_quote green
-set -g fish_color_redirection cyan
+set -g fish_color_redirection yellow
 set -g fish_color_end cyan
 set -g fish_color_error red
 set -g fish_color_param white
 set -g fish_color_comment brblack
 set -g fish_color_selection --background=brblack
-set -g fish_color_operator magenta
+set -g fish_color_operator yellow
 set -g fish_color_escape magenta
 set -g fish_color_autosuggestion brblack
 set -g fish_color_cancel brblack
@@ -16,6 +16,9 @@ set -g fish_color_search_match --background=brblack
 
 set -g fish_pager_color_progress white --background=brblack
 set -g fish_pager_color_background brblack
-set -g fish_pager_color_prefix green
+set -g fish_pager_color_prefix green --bold
 set -g fish_pager_color_completion white
 set -g fish_pager_color_description yellow
+set -g fish_pager_color_selected_prefix green --bold
+set -g fish_pager_color_selected_completion white --bold
+set -g fish_pager_color_selected_description yellow --bold
