dcfa7939324ff5df95ccb52187ef9657a7f93505
Markus Engelbrecht <git@enbre.com>
2023-03-12 10:48:16 +0100

    fish: Improve fzf utilities

--- config/fish/conf.d/37-fzf.fish
+++ config/fish/conf.d/37-fzf.fish
@@ -7,16 +7,19 @@ set -gx FZF_CTRL_T_COMMAND "$FZF_DEFAULT_COMMAND"
 set -gx FZF_DEFAULT_OPTS '--cycle --layout=reverse --border --color=dark,fg:-1,bg:-1,hl:#c678dd,fg+:#ffffff,bg+:#4b5263,hl+:#d858fe,info:#98c379,prompt:#61afef,pointer:#be5046,marker:#e5c07b,spinner:#61afef,header:#61afef'
 
 function vf
-    set -l out $(fzf +i --exact --preview 'bat --style=full --color=always {}' --preview-window 'down,~4,+{2}+4/2')
+    set -l out $(fzf +i --multi --exact --preview 'bat --style=full --color=always --line-range=:200 {}' --preview-window 'down,~4,+{2}+4/2')
     if test $status -eq 0
-        v "$out"
+        v $out
     end
 end
 
 function vfi
-    set -l out $(rg . --line-number --column --no-heading | fzf --exact --delimiter : --preview 'bat --style=full --color=always --highlight-line {2} {1}' --preview-window 'down,~4,+{2}+4/2')
+    set -l out $(rg --line-number --column --no-heading $argv "." | fzf --multi --exact --delimiter : --preview 'bat --style=full --color=always --highlight-line {2} {1}' --preview-window 'down,~4,+{2}+4/2')
     if test $status -eq 0
-        v "$(string join ":" $(string split : -m 4 -f 1-3 "$out"))"
+        for i in (seq (count $out))
+            set out[$i] "$(string join ":" $(string split : -m 4 -f 1-3 "$out[$i]"))" 
+        end
+        v $out
     end
 end
 
