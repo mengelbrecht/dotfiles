ddf5c6c6cad55f8af436232b424e284504cbab55
Markus Engelbrecht <git@enbre.com>
2024-07-17 20:23:10 +0200

    fish: Update prompt theme to not require nerdfonts

--- config/fish/functions/fish_right_prompt.fish
+++ config/fish/functions/fish_right_prompt.fish
@@ -4,15 +4,13 @@ function fish_right_prompt
     end
 
     if test -n "$SSH_TTY"
-        echo -n (set_color $fish_color_redirection)'⟨'
-        echo -n (set_color $fish_color_user)"   $USER"
-        echo -n (set_color $fish_color_cwd)' ⟨'
-        echo -n (set_color $fish_color_host)'   '(prompt_hostname)
+        echo -n (set_color $fish_color_redirection)'⟨ '
+        echo -n (set_color $fish_color_user)$USER(set_color normal)'@'(set_color $fish_color_host)(prompt_hostname)
     end
 
     if test $CMD_DURATION -gt 2000
-        echo -n (set_color $fish_color_option)' ⟨'
-        echo -n (set_color normal)' 󱦟 '(set_color yellow)$CMD_DURATION'ms'
+        echo -n (set_color $fish_color_option)' ⟨ '
+        echo -n (set_color yellow)$CMD_DURATION'ms'
     end
 
     set_color normal
