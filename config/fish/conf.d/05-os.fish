de9af285817f6a0f62e6a9ef15b875e468e7dd9b
Markus Engelbrecht <git@enbre.com>
2024-07-23 21:18:03 +0200

    fish: Add base64 alias for macOS

--- config/fish/conf.d/05-os.fish
+++ config/fish/conf.d/05-os.fish
@@ -3,6 +3,7 @@ switch (uname)
         alias o open
         alias clearDNS "sudo dscacheutil -flushcache; sudo killall -HUP mDNSResponder"
         alias code "/Applications/Visual\ Studio\ Code.app/Contents/Resources/app/bin/code"
+        alias b64 'base64 -b 76 -i'
     case Linux
     case '*'
 end
