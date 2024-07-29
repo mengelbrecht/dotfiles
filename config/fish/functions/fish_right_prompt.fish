9acc0715aada3adb66982d79618929d48d2186ef
Markus Engelbrecht <git@enbre.com>
2024-07-30 00:31:35 +0200

    fish: Add git information to right prompt

--- config/fish/functions/fish_right_prompt.fish
+++ config/fish/functions/fish_right_prompt.fish
@@ -8,9 +8,18 @@ function fish_right_prompt
         echo -n (set_color $fish_color_user)$USER(set_color normal)'@'(set_color $fish_color_host)(prompt_hostname)
     end
 
+    if command -sq git; and test -f '.git/HEAD'
+        set -l ref (string replace -r '.*/' '' (cat '.git/HEAD'))
+        echo -n (set_color $fish_color_cwd)' ⟨ '(set_color normal)''(set_color $fish_color_param)' '$ref
+        git diff --quiet --ignore-submodules HEAD 2> /dev/null
+        if test $status -eq 1
+            echo -n (set_color $fish_color_error)'*'
+        end
+    end
+
     if test $CMD_DURATION -gt 2000
         echo -n (set_color $fish_color_option)' ⟨ '
-        echo -n (set_color normal)'⏳ '(set_color yellow)$CMD_DURATION'ms'
+        echo -n (set_color normal)'⏳ '(set_color $fish_color_cwd)$CMD_DURATION'ms'
     end
 
     set_color normal
