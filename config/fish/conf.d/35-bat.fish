a6aa3790e87d47310322c0b4dfba4d8a9561bf62
Markus Engelbrecht <git@enbre.com>
2024-12-16 16:04:02 +0100

    fish: Update manpage viewer setting

--- config/fish/conf.d/35-bat.fish
+++ config/fish/conf.d/35-bat.fish
@@ -3,6 +3,6 @@ if not type -q bat
 end
 
 set -gx BAT_THEME Catppuccin-macchiato
-set -gx MANPAGER "sh -c 'col -bx | bat -l man -p'"
+set -gx MANPAGER "sh -c 'sed -u -e \"s/\\x1B\[[0-9;]*m//g; s/.\\x08//g\" | bat -l man -p'"
 
 alias cat bat
