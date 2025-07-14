3d552f2ef025cdfc61b33d2c1c9daca90ce82577
Markus Engelbrecht <git@enbre.com>
2025-07-14 22:13:53 +0200

    fish: Fix XDG_RUNTIME_DIR on linux

--- config/fish/conf.d/00-env.fish
+++ config/fish/conf.d/00-env.fish
@@ -7,7 +7,12 @@ set -gx LC_ALL "$LANG"
 set -gx XDG_CONFIG_HOME "$HOME/.config"
 set -gx XDG_DATA_HOME "$HOME/.local/share"
 set -gx XDG_CACHE_HOME "$HOME/.cache"
-set -gx XDG_RUNTIME_DIR "$TMPDIR"
+switch (uname)
+    case Darwin
+        set -gx XDG_RUNTIME_DIR "$TMPDIR"
+    case Linux
+        set -gx XDG_RUNTIME_DIR "/run/user/$(id -u)"
+end
 
 if test -d /usr/local/bin
     fish_add_path /usr/local/bin
